a3d8556d4e583d8ab772eaa1d14d0cc94cedf794
Vladimir <jlemix@mail.ru>
2018-09-16 19:04:15 +0700

    Update quadratic_equation.hs

--- quadratic_equation.hs
+++ quadratic_equation.hs
@@ -1,6 +1,4 @@
-sqroot a b c | a == 0 = error "Wrong inputs"
-	     | otherwise = discriminant a b c
-			   where discriminant a b c | b^2 - 4*a*c < 0 = error "No roots"
-						    | b^2 - 4*a*c == 0 = -b / (2*a)
-						    | otherwise = roots x1 x2
-								  where roots x1 x2 = (((-b + sqrt(b^2 - 4*a*c)) / (2*a)), ((-b - sqrt(b^2 - 4*a*c)) / (2*a)))
+discriminant a b c = b^2 - 4*a*c 			   
+sqroot a b c | (discriminant a b c) < 0 = error "No roots"
+	     | (discriminant a b c) == 0 = ((-b / (2*a)), (-b / (2*a)))
+	     | otherwise = (((-b - sqrt(discriminant a b c)) / (2*a)), ((-b + sqrt(discriminant a b c)) / (2*a)))
