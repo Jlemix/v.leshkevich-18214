419d7445a2ed2d518ca330664df33c1c8c63a1ef
Vladimir <jlemix@mail.ru>
2018-09-24 19:38:30 +0700

    Update quadratic_equation.hs

--- quadratic_equation.hs
+++ quadratic_equation.hs
@@ -1,9 +1,9 @@
 sqroot a b c | ((a == 0) && (b /= 0)) = ((-c / b),(-c / b))
 	     | ((a == 0) && (b == 0)) = error "No roots"
 	     | otherwise = (koren1,koren2)
-			  where
-			    discriminant a b c = b^2 - 4*a*c
-			    drot = if ((discriminant a b c) < 0) then error "No roots" 
-			    	else sqrt(discriminant a b c)
-			    koren1 = (-b + (drot)) / (2 * a)
-			    koren2 = (-b - (drot)) / (2 * a)
+			 where
+				discriminant a b c = b^2 - 4*a*c
+				drot = if ((discriminant a b c) < 0) then error "No roots" 
+				     else sqrt(discriminant a b c)
+				(koren1,koren2) = ((-b + (drot)) / (2 * a), (-b - (drot)) / (2 * a))
+
