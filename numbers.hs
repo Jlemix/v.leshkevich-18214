8e180ce3d963232a9b43d9b15cb8bd688aaff4dd
Vladimir <jlemix@mail.ru>
2018-10-20 22:27:18 +0700

    Update numbers.hs

--- numbers.hs
+++ numbers.hs
@@ -2,15 +2,15 @@
 
 toDecimal :: Char->String->String
 toDecimal base snumber | base == '1' = turing (snumber) 0
-					   | base > '1' && base <= '9' = toDecimalFunc (fromEnum base - 48) (reverse snumber) 0 1 -- ord?
-					   | base >= 'A' && base <= 'Z' = toDecimalFunc (fromEnum base - 29) (reverse snumber) 0 1
-					   | base >= 'a' && base <= 'z' = toDecimalFunc (fromEnum base - 87) (reverse snumber) 0 1
-					   | otherwise = error "Wrong Base"
-								where
-									turing [] length = show length
-									turing (hed:snumber) length = turing snumber (length + 1)
-									toDecimalFunc base [] num decim = show num
-									toDecimalFunc base (hed:snumber) num decim | hed >= '0' &&  hed <= '9' && fromEnum hed - 48 < base = toDecimalFunc base snumber (num + (decim *(fromEnum hed - 48))) (decim * base)
+		       | base > '1' && base <= '9' = toDecimalFunc (fromEnum base - 48) (reverse snumber) 0 1 -- ord?
+		       | base >= 'A' && base <= 'Z' = toDecimalFunc (fromEnum base - 29) (reverse snumber) 0 1
+		       | base >= 'a' && base <= 'z' = toDecimalFunc (fromEnum base - 87) (reverse snumber) 0 1
+		       | otherwise = error "Wrong Base"
+				where
+				    turing [] length = show length
+				    turing (hed:snumber) length = turing snumber (length + 1)
+				    toDecimalFunc base [] num decim = show num
+				    toDecimalFunc base (hed:snumber) num decim | hed >= '0' &&  hed <= '9' && fromEnum hed - 48 < base = toDecimalFunc base snumber (num + (decim *(fromEnum hed - 48))) (decim * base)
 																			   | hed >= 'A' &&  hed <= 'Z' && fromEnum hed - 29 < base = toDecimalFunc base snumber (num + (decim *(fromEnum hed - 29))) (decim * base)
 																			   | hed >= 'a' &&  hed <= 'z' && fromEnum hed - 87 < base = toDecimalFunc base snumber (num + (decim *(fromEnum hed - 87))) (decim * base)
 																			   | otherwise = error "Wrong Number"
@@ -21,21 +21,21 @@ toDecimal base snumber | base == '1' = turing (snumber) 0
 																		   
 fromDecimal :: Char->String->String
 fromDecimal toBase snumber | toBase == '1' = turing ['1'] (read snumber::Int) 
-						   | toBase > '1' && toBase <= '9' = fromDecimalFunc (fromEnum toBase - 48) (read snumber::Int) []
-						   | toBase >= 'A' && toBase <= 'Z' = fromDecimalFunc (fromEnum toBase - 29) (read snumber::Int) []
-					       | toBase >= 'a' && toBase <= 'z' = fromDecimalFunc (fromEnum toBase - 87) (read snumber::Int) []
-					       | otherwise = error "Wrong Base"
-								where
-									turing num 0 = num
-									turing num length = turing (num ++ "1") (length - 1)
-									fromDecimalFunc :: Int -> Int -> String -> String
-									fromDecimalFunc base 0 num = num
-									fromDecimalFunc base snumber num = fromDecimalFunc base (div snumber base) ((toEnum(asci (mod snumber base) )::Char):num)
-										where
-											asci numba | numba < 10 = numba + 48
-													   | numba > 9 && numba < 36 = numba + 87
-													   | numba > 35 && numba < 62 = numba + 29
-													   | otherwise = error "Not in range"
+			   | toBase > '1' && toBase <= '9' = fromDecimalFunc (fromEnum toBase - 48) (read snumber::Int) []
+			   | toBase >= 'A' && toBase <= 'Z' = fromDecimalFunc (fromEnum toBase - 29) (read snumber::Int) []
+			   | toBase >= 'a' && toBase <= 'z' = fromDecimalFunc (fromEnum toBase - 87) (read snumber::Int) []
+			   | otherwise = error "Wrong Base"
+				    where
+					turing num 0 = num
+					turing num length = turing (num ++ "1") (length - 1)
+					fromDecimalFunc :: Int -> Int -> String -> String
+					fromDecimalFunc base 0 num = num
+					fromDecimalFunc base snumber num = fromDecimalFunc base (div snumber base) ((toEnum(asci (mod snumber base) )::Char):num)
+						where
+						    asci numba | numba < 10 = numba + 48
+							       | numba > 9 && numba < 36 = numba + 87
+							       | numba > 35 && numba < 62 = numba + 29
+							       | otherwise = error "Not in range"
 --Example:													   
 --fromDecimal '2' "4" => fromDecimalFunc (50 - 48) (4) [] => fromDecimalFunc 2 4 [] => fromDecimalFunc 2 (2) ((toEnum(48)::Char):num) <- ('0' : []) => ... => fromDecimalFunc 2 0 "100" = "100"
 
