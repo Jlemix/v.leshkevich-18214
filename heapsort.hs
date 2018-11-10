c291330695a70b4ad8cec97d3aaf32666a7bc463
Vladimir <jlemix@mail.ru>
2018-11-10 15:02:18 +0700

    Update heapsort.hs

--- heapsort.hs
+++ heapsort.hs
@@ -1,13 +1,10 @@
-swap :: Int -> Int -> [a] -> [a]
-swap m n list = take (i1 - 1) list ++ [list !! (i2 - 1)] ++ (drop i1 (take (i2 - 1) list)) ++ [list !! (i1 - 1)] ++ drop i2 list
-    where i1 = min m n
-          i2 = max m n
-
 heapSort :: [Int] -> [Int]
 heapSort [] = []
 heapSort xs = heapSort (take (length xs - 1) (swapped xs)) ++ [head (buildMaxHeap xs)]
-    where 
+    where		
 		swapped xs = swap 1 (length xs) (buildMaxHeap xs)
+		swap :: Int -> Int -> [a] -> [a]
+		swap m n list = take ((min m n) - 1) list ++ [list !! ((max m n) - 1)] ++ (drop m (take ((max m n) - 1) list)) ++ [list !! ((min m n) - 1)] ++ drop (max m n) list
 		buildMaxHeap :: [Int] -> [Int]
 		buildMaxHeap xs = foldr heapify xs ([1 .. (length xs) `div` 2])
 			where
@@ -19,4 +16,3 @@ heapSort xs = heapSort (take (length xs - 1) (swapped xs)) ++ [head (buildMaxHea
 						big_iLeftChild i j = if iLeftChild i <= length arr && (arr !! ((iLeftChild i) - 1)) > (arr !! (j-1)) then iLeftChild i else j
 						big_iRightChild i j = if iRightChild i <= length arr && (arr !! ((iRightChild i) - 1)) > (arr !! (j-1)) then iRightChild i else j
 						largest i = big_iRightChild i (big_iLeftChild i i)
-						
