7da14e0eee8c01a9923e78aca7e5bba00e792726
Vladimir <jlemix@mail.ru>
2018-11-11 19:13:30 +0700

    Update heapsort.hs

--- heapsort.hs
+++ heapsort.hs
@@ -3,14 +3,14 @@ heapSort [] = []
 heapSort xs = heapSort (take (length xs - 1) (swapped xs)) ++ [head (buildMaxHeap xs)]
     where		
 		swapped xs = swap 1 (length xs) (buildMaxHeap xs)
-		swap :: Int -> Int -> [a] -> [a]
+		swap :: Int -> Int -> [a] -> [a] --Swap the first element of the list with the final element
 		swap m n list = take ((min m n) - 1) list ++ [list !! ((max m n) - 1)] ++ (drop m (take ((max m n) - 1) list)) ++ [list !! ((min m n) - 1)] ++ drop (max m n) list
 		buildMaxHeap :: [Int] -> [Int]
-		buildMaxHeap xs = foldr heapify xs ([1 .. (length xs) `div` 2])
+		buildMaxHeap xs = foldr heapify xs ([1 .. (length xs) `div` 2]) --builds a heap from a list
 			where
-				heapify :: Int -> [Int] -> [Int]
+				heapify :: Int -> [Int] -> [Int] --Build the heap in array so that largest value is at the root
 				heapify i arr = if largest i /= i then heapify (largest i) (swap (largest i) i arr) else arr
-					where
+					where --leafSearch
 						iLeftChild i = 2*i
 						iRightChild i = 2*i + 1
 						big_iLeftChild i j = if iLeftChild i <= length arr && (arr !! ((iLeftChild i) - 1)) > (arr !! (j-1)) then iLeftChild i else j
