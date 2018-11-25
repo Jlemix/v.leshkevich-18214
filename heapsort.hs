4e450683a3ca8803b95760bc710e1e79a2dc6272
Vladimir <jlemix@mail.ru>
2018-11-25 17:19:40 +0700

    FIX (NOW WORKS WITH STRINGS)
    worked wrongly with strings until current update.

--- heapsort.hs
+++ heapsort.hs
@@ -1,14 +1,14 @@
-heapSort :: [Int] -> [Int]
+heapSort :: Ord a => [a] -> [a]
 heapSort [] = []
 heapSort xs = heapSort (take (length xs - 1) (swapped xs)) ++ [head (buildMaxHeap xs)]
-    where		
+    where 
+		--swap :: Int -> Int -> [a] -> [a] --Swap the first element of the list with the final element
+		swap m n list = take ((min m n) - 1) list ++ [list !! ((max m n) - 1)] ++ (drop (min m n) (take ((max m n) - 1) list)) ++ [list !! ((min m n) - 1)] ++ drop (max m n) list
 		swapped xs = swap 1 (length xs) (buildMaxHeap xs)
-		swap :: Int -> Int -> [a] -> [a] --Swap the first element of the list with the final element
-		swap m n list = take ((min m n) - 1) list ++ [list !! ((max m n) - 1)] ++ (drop m (take ((max m n) - 1) list)) ++ [list !! ((min m n) - 1)] ++ drop (max m n) list
-		buildMaxHeap :: [Int] -> [Int]
+		--buildMaxHeap :: [Int] -> [Int]
 		buildMaxHeap xs = foldr heapify xs ([1 .. (length xs) `div` 2]) --builds a heap from a list
 			where
-				heapify :: Int -> [Int] -> [Int] --Build the heap in array so that largest value is at the root
+				--heapify :: Int -> [Int] -> [Int] --Build the heap in array so that largest value is at the root
 				heapify i arr = if largest i /= i then heapify (largest i) (swap (largest i) i arr) else arr
 					where --leafSearch
 						iLeftChild i = 2*i
