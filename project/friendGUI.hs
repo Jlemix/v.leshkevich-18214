541a07aa6498a7b15745f6a4ed9b1c61042ad2a8
Vladimir <jlemix@mail.ru>
2019-03-23 16:59:59 +0700

    added tie condition

--- project/friendGUI.hs
+++ project/friendGUI.hs
@@ -18,11 +18,16 @@ winP1 = animate (InWindow "ETO KRESTI" (800, 300) (100, 100)) white winningPictu
 
 winP2 = animate (InWindow "ETO KRESTI" (800, 300) (100, 100)) white winningPictureP2
 
+noOne = animate (InWindow "ETO KRESTI" (800, 300) (100, 100)) white tiePicture
+
 winningPictureP1 :: Float -> Picture
 winningPictureP1 _ = translate (-300) (5) $ text "X WON"
 
 winningPictureP2 :: Float -> Picture
 winningPictureP2 _ = translate (-300) (5) $ text "O WON"
+
+tiePicture :: Float -> Picture
+tiePicture _ = translate (-300) (5) $ text "TIE"
 
 main :: IO ()
 main = do
@@ -68,7 +73,7 @@ handleInput
       Nothing -> do
         let newField = (ix gridX . ix gridY .~ (Just X)) field -- novoe pole s 'X' gde najal polzovatel, menyaem hod, ix dlya indeksirovaniya spiska, tipo obhod zadannogo indeksa
         when (winCond newField (maybeContainer X) == True) (winP1)
-
+        when (tie newField (maybeContainer X) == True) (noOne)
         return (newField, O)
 handleInput
   (EventKey (MouseButton LeftButton) Up _ (x, y))
@@ -83,16 +88,19 @@ handleInput
       Nothing -> do
         let newField = (ix gridX . ix gridY .~ (Just O)) field
         when (winCond newField (maybeContainer O) == True) (winP2)
-
+        when (tie newField (maybeContainer O) == True) (noOne)
         return (newField, X)
 handleInput _ (field, mark) = return (field, mark)
 
 playerTurn :: Float -> (Field, Mark) -> IO (Field, Mark)
-playerTurn _ = return
+playerTurn _  = return
 
 maybeContainer :: a -> Maybe a
 maybeContainer x = Just x
 maybeContainer _ = Nothing
+
+tie :: Field -> Maybe Mark -> Bool
+tie f m = if ((f!!0!!0 /= Nothing) && (f!!0!!1 /= Nothing) && (f!!0!!2 /= Nothing) && (f!!1!!0 /= Nothing) && (f!!1!!1 /= Nothing) && (f!!1!!2 /= Nothing) && (f!!2!!0 /= Nothing) && (f!!2!!1 /= Nothing) && (f!!2!!2 /= Nothing)) == True then True else False
 
 winCond :: Field -> Maybe Mark -> Bool
 winCond f c | ((f!!0!!0 == c) && (f!!0!!1 == c) && (f!!0!!2 == c)) == True = True
